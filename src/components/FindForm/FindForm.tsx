58811a27b2211de8eff4836c1f5769c6d87b62af
xrisent <ormonov31261@gmail.com>
2025-03-27 17:35:22 +0600

    fetch characters

--- src/components/FindForm/FindForm.tsx
+++ src/components/FindForm/FindForm.tsx
@@ -1,11 +1,34 @@
+"use client"
+
+import { useState } from "react";
+import { fetchCharacters } from "@/features/rickAndMortyApiGet";
 import { CustomButton } from "@/shared/CustomButton/CustomButton";
 import { CustomInput } from "@/shared/CustomInput/CustomInput";
 
-export const FindForm: React.FC<{}> = () => {
+export const FindForm: React.FC = () => {
+    const [name, setName] = useState("");
+    const [characters, setCharacters] = useState<any[]>([]);
+
+    const handleSearch = async () => {
+        const data = await fetchCharacters(name, 1);
+        if (data) {
+            setCharacters(data.results);
+        }
+    };
+
     return (
-        <form className="m-10">
-            <CustomInput/>
-            <CustomButton/>
-        </form>
-    )
+        <div className="m-10">
+            <form onSubmit={(e) => { e.preventDefault(); handleSearch(); }}>
+                <CustomInput value={name} onChange={(e) => setName(e.target.value)} />
+                <CustomButton type="submit" />
+            </form>
+
+            <div className="mt-5">
+                {characters.map((char) => (
+                    <div key={char.id} className="p-2 border-b">{char.name}</div>
+                ))}
+            </div>
+
+        </div>
+    );
 };
